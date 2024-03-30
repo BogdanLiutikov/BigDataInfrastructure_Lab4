f5b8bd648ceb6d2cda7ea415f29fe4b7a8ae1a08
Bogdan Liutikov <lbi2001@yandex.ru>
2024-03-31 02:30:19 +0300

    predict test

--- src/predict.py
+++ src/predict.py
@@ -4,6 +4,7 @@ import sys
 from configparser import ConfigParser
 
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator
 
 
@@ -21,21 +22,49 @@ class Predictor():
                                  default="RandomForestClassifier",
                                  nargs="?",
                                  choices=["RandomForestClassifier"])
+        self.parser.add_argument("-t",
+                                 "--tests",
+                                 type=str,
+                                 help="Select tests",
+                                 required=False,
+                                 default="smoke",
+                                 const="smoke",
+                                 nargs="?",
+                                 choices=["smoke", "func"])
 
-    def predict(self, vector) -> np.array:
-        vector = np.array(vector)
         args = self.parser.parse_args()
         try:
             model_path = self.config.get('models.fitted', args.model)
             with open(model_path, "rb") as model:
-                classifier: BaseEstimator = pickle.load(model)
+                self.model: BaseEstimator = pickle.load(model)
         except FileNotFoundError as e:
             print(e)
             sys.exit(1)
 
-        return classifier.predict(vector)
+    def predict(self, vector) -> np.array:
+        vector = np.array(vector)
+        return self.model.predict(vector)
+
+    def test(self):
+        data_split = self.config['data.splited']
+        x_test_path = data_split['x_test']
+        y_test_path = data_split['y_test']
+
+        x_test = pd.read_csv(x_test_path)
+        y_test = pd.read_csv(y_test_path).iloc[:, 0]
+
+        predicts = self.model.predict(x_test)
+        args = self.parser.parse_args()
+        if args.tests == "smoke":
+            try:
+                score = self.model.score(x_test, y_test)
+                print(f'{args.model} has {score} score')
+            except Exception as e:
+                print(e)
+                sys.exit(1)
+            print(f'{args.model} passed smoke tests')
 
 
 if __name__ == "__main__":
     predictor = Predictor()
-    print(predictor.predict([[1, 2, 3, 4]]))
+    predictor.test()
